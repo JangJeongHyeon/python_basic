e204f0c575d9c60e049f1c95c4c7c7f3b0671ba1
JohnMark <practice1356@gmail.com>
2018-02-07 19:14:20 +0900

    python data structure

---
+++ basic_datatype/list/list_generate.py
@@ -0,0 +1,26 @@
+# variable binding a memory of has some value.
+interset1 = "삼성전자"
+interset2 = "LG전자"
+interset3 = "네이버"
+interset4 = "카카오"
+
+# printing value of memory using by call variable
+print(interset1)
+print(interset2)
+print(interset3)
+print(interset4)
+
+# using list structure for binding a lot of value
+interset = ["삼성전자", "LG전자", "네이버", "카카오"]
+print(interset)
+
+# list is easy handle sequence data
+daishin = [9130, 9150, 9150, 9300, 9400]
+
+# difference type biding as list
+mystock = ['Naver', 5000]
+print(mystock)
+
+# create empty list object
+empty = []
+print(empty)
--- variable_and_string/basic_datatype/datatype.py
+++
@@ -1,3 +0,0 @@
-print(type(70000))
-print(type(3.141592))
-print(type('python'))
