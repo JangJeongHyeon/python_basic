3e98fffd83b6ffeda3c942b546a7ec534b86a91c
JangJeongHyeon <practice1356@gmail.com>
2018-05-19 00:18:13 +0900

    create menu printing function

--- _project/contact/contact.py
+++ _project/contact/contact.py
@@ -11,7 +11,7 @@ class Contact:
         print('E-mail: ', self.e_mail)
         print('Address: ', self.address)
 
-# add
+""" create new contact class instance """
 def new_contact():
     name = input('Name: ')
     phone_number = input('Phone Number: ')
@@ -19,11 +19,20 @@ def new_contact():
     address = input('Address: ')
     return Contact(name, phone_number, e_mail,address)
 
+def print_menu():
+    print('1. 연락처 입력')
+    print('2. 연락처 출력')
+    print('3. 연락처 삭제')
+    print('4. 종료')
+    menu = input("메뉴선택: ")
+    return int(menu)
+
+
 def run():
-    # kim = Contact('JohnMark','010-1234-1234','practice1356@gmail.com','Anseong')
-    # kim.print_info()
-    contact = new_contact()
-    contact.print_info()
+    while True:
+        menu = print_menu()
+        if menu == 4:
+            break
 
     
 if __name__ == '__main__':
